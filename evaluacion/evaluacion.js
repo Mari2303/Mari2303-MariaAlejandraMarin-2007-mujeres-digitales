b9ab01f21942bd23103721e621645c90490cdcc7
Mari2303 <mariaalejandramarinhenriquez@gmail.com>
2024-10-29 20:52:17 -0500

    Add files via upload

---
+++ evaluacion/evaluacion.js
@@ -0,0 +1,14 @@
+ ^[()`~^*/?`[()^+`-~()#[$()/~()
+  %[]()}+[()[{=~¿()=$?%?!?]¡~()
+  ¿()`¬^~()[{=~()?+^[]=?
+  ()^{()*~¡[$[{()¡[()?=[]`¬]()¿()[{;+
+  [$__~()`~]=]+~
+
+
+  MI SUPER RESPUESTA !!
+
+
+  me complace mucho ver lo 
+  bien que estoy trabajando y como esto aumenta mis
+  poderes de atención y esfuerzo continuo
+  
